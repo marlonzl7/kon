04f38f2f99e2e8cf383bca06ad3f730149377b97
marlonzl7 <98499596+marlonzl7@users.noreply.github.com>
2025-04-05 14:16:28 -0300

    adicionar métodos existentes a interface Converter para padronização

--- src/main/java/com/marlonzl7/kon/converters/Converter.java
+++ src/main/java/com/marlonzl7/kon/converters/Converter.java
@@ -1,5 +1,9 @@
 package com.marlonzl7.kon.converters;
 
+import java.util.List;
+
 public interface Converter {
     public double convert(String from, String to, double value);
+    public List<String> getSupportedConversions();
+    public String getConversionByIndex(int index);
 }
