354cc589346c6690cbfa79223624d78e1c4ef531
marlonzl7 <98499596+marlonzl7@users.noreply.github.com>
2025-04-06 05:08:01 -0300

    refactor: removendo métodos que são implementados como estáticos em suas respectivas classes

--- src/main/java/com/marlonzl7/kon/converters/Converter.java
+++ src/main/java/com/marlonzl7/kon/converters/Converter.java
@@ -4,6 +4,4 @@ import java.util.List;
 
 public interface Converter {
     public double convert(String from, String to, double value);
-    public List<String> getSupportedConversions();
-    public String getConversionByIndex(int index);
 }
