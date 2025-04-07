b491a6181d4aba649d7d33d1de4c8a9f998adc85
marlonzl7 <marlondesouzasampaio@gmail.com>
2025-04-07 13:04:23 -0300

    feat(commands): adicionar ListCommand

---
+++ src/main/java/com/marlonzl7/kon/commands/ListCommand.java
@@ -0,0 +1,55 @@
+package com.marlonzl7.kon.commands;
+
+import com.marlonzl7.kon.converters.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ListCommand implements Command {
+    @Override
+    public void execute(Map<String, String> arguments) {
+        String type = getKeyValue(arguments);
+
+        if (type == null || type.isBlank()) {
+            System.err.println("Error: no conversion type specified.");
+            System.err.println("Use -l or --list followed by a value");
+            System.err.println("Example: kon --list speed");
+            return;
+        }
+
+        type = type.trim().toLowerCase();
+
+        switch (type) {
+            case "length" -> printSupportedConverions("Length", LengthConverter.getSupportedConversions());
+            case "mass" -> printSupportedConverions("Mass", MassConverter.getSupportedConversions());
+            case "speed" -> printSupportedConverions("Speed", SpeedConverter.getSupportedConversions());
+            case "temperature" -> printSupportedConverions("Temperature", TemperatureConverter.getSupportedConversions());
+            case "time" -> printSupportedConverions("Time", TimeConverter.getSupportedConversions());
+            case "volume" -> printSupportedConverions("Volume", VolumeConverter.getSupportedConversions());
+            default -> throw new IllegalArgumentException("Invalid argument");
+        }
+    }
+
+    private String getKeyValue(Map<String, String> arguments) {
+        if (arguments.containsKey("-l")) {
+            return arguments.get("-l");
+        }
+
+        if (arguments.containsKey("--list")) {
+            return arguments.get("--list");
+        }
+
+        return null;
+    }
+
+    private void printSupportedConverions(String type, List<String> conversions) {
+        System.out.println("--- " + type.toUpperCase() + " CONVERSIONS ---");
+        for (String conversion : conversions) {
+            String[] parts = conversion.split("->");
+            if (parts.length == 2) {
+                System.out.printf(" - %s to %s%n", parts[0], parts[1]);
+            }
+        }
+    }
+}
