e7a4aebb359e7b094658e3441653289ff4dfd712
marlonzl7 <98499596+marlonzl7@users.noreply.github.com>
2025-04-07 13:40:05 -0300

    feat(commands): adicionar HelpCommand

---
+++ src/main/java/com/marlonzl7/kon/commands/HelpCommand.java
@@ -0,0 +1,42 @@
+package com.marlonzl7.kon.commands;
+
+import java.util.Map;
+
+public class HelpCommand implements Command {
+
+    @Override
+    public void execute(Map<String, String> arguments) {
+        System.out.println("Kon - Conversor de Unidades");
+        System.out.println();
+
+        System.out.println("Uso:");
+        System.out.println("kon --converter <grandeza> --from <unidade> --to <unidade> --value <valor>");
+        System.out.println("kon --list <grandeza>");
+        System.out.println("kon --help");
+        System.out.println();
+
+        System.out.println("Opções:");
+        System.out.println("-c, --converter <grandeza>   Define o tipo de grandeza (ex: massa, comprimento, temperatura)");
+        System.out.println("-f, --from <unidade>         Unidade de origem (ex: kg, m, C)");
+        System.out.println("-t, --to <unidade>           Unidade de destino (ex: g, cm, F)");
+        System.out.println("-v, --value <valor>          Valor numérico a ser convertido");
+        System.out.println("-l, --list <grandeza>        Lista as unidades disponíveis para grandeza informada");
+        System.out.println("-h, --help                   Exibe esta mensagem de ajuda");
+        System.out.println();
+
+        System.out.println("Exemplos:");
+        System.out.println("kon --converter speed --from ms --to kmh --value 25");
+        System.out.println("    Converte 25m/s para km/h.");
+        System.out.println();
+        System.out.println("kon -c temperature -f celsius -t fahrenheit -v 32");
+        System.out.println("    Converte 32 graus Celsius para Fahrenheit.");
+        System.out.println();
+        System.out.println("kon --list length");
+        System.out.println("    Lista todas as unidades disponíveis para a grandeza 'length'.");
+        System.out.println();
+
+        System.out.println("Notas:");
+        System.out.println(" - Os nomes das grandezas devem ser escritos em minúsculas (ex: mass, temperature)");
+        System.out.println(" - Utilize 'kon --list' para descobrir quais unidades estão disponíveis para conversão");
+    }
+}
