ea019f8776934d89375269ac184356b8d1cff708
marlonzl7 <marlondesouzasampaio@gmail.com>
2025-04-08 06:40:25 -0300

    feat(commands): adicionar VolumeConvertCommand

---
+++ src/main/java/com/marlonzl7/kon/commands/VolumeConvertCommand.java
@@ -0,0 +1,20 @@
+package com.marlonzl7.kon.commands;
+
+import com.marlonzl7.kon.converters.Converter;
+import com.marlonzl7.kon.converters.VolumeConverter;
+
+import java.util.Map;
+
+public class VolumeConvertCommand implements Command {
+    @Override
+    public void execute(Map<String, String> arguments) {
+        String from = arguments.get("from");
+        String to = arguments.get("to");
+        double value = Double.parseDouble(arguments.get("value"));
+
+        Converter converter = new VolumeConverter();
+        double result = converter.convert(from, to, value);
+
+        System.out.printf("Result: %.2f %s -> %.2f %s%n", value, from, result, to);
+    }
+}
