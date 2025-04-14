1a610dec2e585f02ce8278aab1b48577690915cb
marlonzl7 <98499596+marlonzl7@users.noreply.github.com>
2025-04-14 07:32:20 -0300

    style: renomear método de CommandParser

--- src/main/java/com/marlonzl7/kon/Main.java
+++ src/main/java/com/marlonzl7/kon/Main.java
@@ -4,6 +4,6 @@ import com.marlonzl7.kon.core.CommandParser;
 
 public class Main {
     public static void main(String[] args) {
-        CommandParser.run(args);
+        CommandParser.main(args);
     }
 }
