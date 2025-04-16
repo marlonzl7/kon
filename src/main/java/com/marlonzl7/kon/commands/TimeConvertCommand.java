937f510521f17ff3bba48b1499ac5c4a057a4a02
marlonzl7 <marlondesouzasampaio@gmail.com>
2025-04-15 21:34:11 -0300

    style(commands): Traduzir mensagens de resultado para português nas classes de comando

--- src/main/java/com/marlonzl7/kon/commands/TimeConvertCommand.java
+++ src/main/java/com/marlonzl7/kon/commands/TimeConvertCommand.java
@@ -2,6 +2,7 @@ package com.marlonzl7.kon.commands;
 
 import com.marlonzl7.kon.converters.Converter;
 import com.marlonzl7.kon.converters.TimeConverter;
+import com.marlonzl7.kon.utils.TranslationHelper;
 
 import java.util.Map;
 
@@ -15,6 +16,6 @@ public class TimeConvertCommand implements Command {
         Converter converter = new TimeConverter();
         double result = converter.convert(from, to, value);
 
-        System.out.printf("Resultado: %.2f %s -> %.2f %s%n", value, from, result, to);
+        System.out.printf("Resultado: %.2f %s -> %.2f %s%n", value, TranslationHelper.translateUnit(from), result, TranslationHelper.translateUnit(to));
     }
 }
